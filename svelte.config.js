4a612e30bd729034386e35ffe6ae015f02eb654c
Phytoplancton <jonas-ullmann@web.de>
2023-09-29 16:25:10 +0200

    fixed wrong base

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 export default {
-    base: "/chord player/",
+    base: "/chord_player/",
     // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
     // for more information about preprocessors
     preprocess: vitePreprocess(),
