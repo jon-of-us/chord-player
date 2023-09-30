1c6c037246b9f7ff1cba064592beb1a18dc56917
Phytoplancton <jonas-ullmann@web.de>
2023-09-30 14:53:37 +0200

    change repo name

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/chord_player/",
+    base: "/chord-player/",
     plugins: [svelte()],
 });
